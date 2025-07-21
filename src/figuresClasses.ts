fdb81baa3e639b90a46dbbc7b2abcba8d986cfa4
Dmytro Kovtun <kovtundima2910@gmail.com>
2025-07-21 18:32:08 +0300

    i replaced getArea in Circle

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -60,7 +60,9 @@ export class Circle implements Figure {
   }
 
   getArea(): number {
-    return Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
+    const area = Math.PI * this.radius * this.radius;
+
+    return Math.floor(area * 100) / 100;
   }
 }
 
